8320b4f1b041be61b0415c397603ccd05c982bbb
arfisher611 <arfisher611@gmail.com>
2019-06-05 18:47:48 -0400

    Update button.js

--- js/button.js
+++ js/button.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
                 $.ajax({
                     url: endpoint,
                     //Fails due to webpage not supporting JSONP
-                    dataType: "jsonp",
+                    dataType: "json",
                     crossDomain: true,
                     type: "GET",
                     success: function (result, status, xhr){
